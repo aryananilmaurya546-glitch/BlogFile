2f85a01082cad11021de6bdc4897c865759e1868
Aryan Maurya <aryananilmaurya546@gmail.com>
2026-05-14 16:58:31 +0530

    Add files via upload

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+function showForm(){
+    document.querySelector(".form").style.display = 'block';
+    document.querySelector(".buttonForTheAddBlog").style.display = 'none';
+    document.querySelector(".blog").style.display = 'none';
+    document.querySelector(".formForSearch").style.display = 'none';
+}
